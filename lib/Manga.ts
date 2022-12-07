a7b3b9f50c5f66a24e15eb2f8fda3ce42a113c74
Alexandre BREAULT <creepminecraft53@gmail.com>
2022-12-07 23:08:05 +0100

    lib/Manga.ts

--- lib/Manga.ts
+++ lib/Manga.ts
@@ -1,5 +1,6 @@
 export function getManga() {
   const urlApi = process.env.VERCEL_URL + "/api/manga";
+  console.log(urlApi);
   const request = fetch(urlApi);
   const data = request.then((json) => json.json());
   return data;
