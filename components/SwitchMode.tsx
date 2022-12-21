f9754843bcc65cf0d759ed78c9ac10797be88767
Alexandre BREAULT <creepminecraft53@gmail.com>
2022-12-21 14:02:47 +0100

    edition switch Mode

--- components/SwitchMode.tsx
+++ components/SwitchMode.tsx
@@ -1,15 +1,18 @@
 import { useTheme as useNextTheme } from "next-themes";
-import { Switch, Text, useTheme } from "@nextui-org/react";
+import { Switch, useTheme } from "@nextui-org/react";
 import { IoMoonSharp, IoSunnySharp } from "react-icons/io5";
 export default function SwitchMode(): JSX.Element {
   const { setTheme } = useNextTheme();
   const { isDark, type } = useTheme();
   return (
     <div>
-      <Text>{isDark ? <IoMoonSharp /> : <IoSunnySharp />}</Text>
       <Switch
         checked={isDark}
         onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
+        color='default'
+        iconOn={<IoMoonSharp />}
+        iconOff={<IoSunnySharp />}
+        bordered={true}
       />
     </div>
   );
